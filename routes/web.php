7a348ce2b786c7a0923072de63c14397b3c5485b
AGabrielR <50876101+AGabrielR@users.noreply.github.com>
2022-02-16 07:30:27 -0400

    corrigindo nome da rota

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/add/categoria', [CategoriaController::class, 'index'])->name('createCategory');
 
-Route::post('/insert/categoria', [CategoriaController::class, 'store'])->name('insertCategory');
+Route::post('/register/categoria', [CategoriaController::class, 'store'])->name('registerCategory');
