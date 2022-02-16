47331449e1bc48e5cdcd9e4d4e90891437e992e5
AGabrielR <50876101+AGabrielR@users.noreply.github.com>
2022-02-15 22:30:04 -0400

    Inserida tela de criar categorias // Corrigidos erros na rota

--- resources/views/createCategory.blade.php
+++ resources/views/createCategory.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
     <div class="container">
-        <form action="">
-        @csrf
-            <div class="row mb-3">
-                <label class="col-md-4 col-form-label text-md-end" for="">Categoria</label>
-                <div class="col-md-3">
-                    <input type="text" name="categoriaNome" id="" class="form-control">
-                </div>
-                <div class="row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-success">
-                            Criar
-                        </button>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Registrar Usuário') }}</div>
+                        <div class="card-body">
+                            <form action="">
+                            @csrf
+                            <div class="row mb-3">
+                                <label class="col-md-4 col-form-label text-md-end" for="">Categoria</label>
+                                <div class="col-md-3">
+                                    <input type="text" name="categoriaNome" id="" class="form-control">
+                                </div>
+                            </div>
+                                <div class="row mb-0">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-success">
+                                            Criar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 @endsection
