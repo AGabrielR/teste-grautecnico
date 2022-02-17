0831eaa2e5ddaf7379ada74c6cbed5a4d908e07a
AGabrielR <50876101+AGabrielR@users.noreply.github.com>
2022-02-17 13:57:49 -0400

    tentando mudar o method de lugar pra aceitar a rota

--- resources/views/updaters/user.blade.php
+++ resources/views/updaters/user.blade.php
@@ -9,8 +9,8 @@
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('updateUser', $user->id) }}">
+                        @method('PUT')
                         @csrf
-
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nome') }}</label>
 
