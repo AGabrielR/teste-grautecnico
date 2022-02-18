0e118a523ded26bafa086ce920660e0b652890ae
AGabrielR <50876101+AGabrielR@users.noreply.github.com>
2022-02-18 13:53:03 -0400

    Tentando corrigir o delete categoria

--- resources/views/relatorios/categoria.blade.php
+++ resources/views/relatorios/categoria.blade.php
@@ -34,6 +34,7 @@
                             <form action="{{route('deleteCategoria', $categoria->id)}}" method="post">
                                 @csrf
                                 @method('delete')
+                                <input type="hidden" name="id" value="{{$categoria->id}}">
                                 <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
@@ -53,7 +54,7 @@
                                     </div>
                                     <div class="modal-footer">
                                       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                                      <button type="submit" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                      <button type="submit" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">Deletar</button>
                                     </div>
                                   </div>
                                 </div>
